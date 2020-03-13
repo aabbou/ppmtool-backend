2d006b1bc770c744965e82f90177688998dfa16b
aabbou <ayoub.abbou01@gmail.com>
2020-03-13 16:56:26 +0100

    [EVOL] Adding some security config + jwt config and token generation

--- src/main/java/com/aabbou/ppm/exceptions/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/aabbou/ppm/exceptions/CustomResponseEntityExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.aabbou.ppm.exceptions;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,26 +10,35 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 @RestController
-public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class CustomResponseEntityExceptionHandler
+		extends
+			ResponseEntityExceptionHandler {
 
-    @ExceptionHandler
-    public final ResponseEntity<Object> handleProjectIdException(ProjectIdException ex, WebRequest request){
-        ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
-        return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);
-    }
+	@ExceptionHandler
+	public final ResponseEntity<Object> handleProjectIdException(
+			ProjectIdException ex, WebRequest request) {
+		ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(
+				ex.getMessage());
+		return new ResponseEntity<Object>(exceptionResponse,
+				HttpStatus.BAD_REQUEST);
+	}
 
-    @ExceptionHandler
-    public final ResponseEntity<Object> handleProjectNotFoundException(ProjectNotFoundException ex, WebRequest request){
-        ProjectNotFoundExceptionResponse exceptionResponse = new ProjectNotFoundExceptionResponse(ex.getMessage());
-        return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);
-    }
+	@ExceptionHandler
+	public final ResponseEntity<Object> handleProjectNotFoundException(
+			ProjectNotFoundException ex, WebRequest request) {
+		ProjectNotFoundExceptionResponse exceptionResponse = new ProjectNotFoundExceptionResponse(
+				ex.getMessage());
+		return new ResponseEntity<Object>(exceptionResponse,
+				HttpStatus.BAD_REQUEST);
+	}
 
-
-//    @ExceptionHandler
-//    public final ResponseEntity<Object> handleUsernameAlreadyExists(UsernameAlreadyExistsException ex, WebRequest request){
-//        UsernameAlreadyExistsResponse exceptionResponse = new UsernameAlreadyExistsResponse(ex.getMessage());
-//        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
-//    }
-
+	@ExceptionHandler
+	public final ResponseEntity<Object> handleUsernameAlreadyExists(
+			UsernameAlreadyExistsException ex, WebRequest request) {
+		UsernameAlreadyExistsResponse exceptionResponse = new UsernameAlreadyExistsResponse(
+				ex.getMessage());
+		return new ResponseEntity<Object>(exceptionResponse,
+				HttpStatus.BAD_REQUEST);
+	}
 
 }
