51eac419a22cc739622c8748fc7d6c61e1cb48d1
aabbou <ayoub.abbou01@gmail.com>
2020-03-14 13:16:05 +0100

    [EVOL] adding token expiration date

--- src/main/java/com/aabbou/ppm/config/security/SecurityConstants.java
+++ src/main/java/com/aabbou/ppm/config/security/SecurityConstants.java
@@ -2,10 +2,10 @@ package com.aabbou.ppm.config.security;
 
 public class SecurityConstants {
 
-	public static final String SIGN_UP_URLS = "/api/users/**";
-	public static final String H2_URL = "h2-console/**";
-	public static final String SECRET = "SecretKeyToGenJWTs";
-	public static final String TOKEN_PREFIX = "Bearer ";
-	public static final String HEADER_STRING = "Authorization";
-	public static final long EXPIRATION_TIME = 30_000; // 30 seconds
+    public static final String SIGN_UP_URLS = "/api/users/**";
+    public static final String H2_URL = "h2-console/**";
+    public static final String SECRET = "SecretKeyToGenJWTs";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final long EXPIRATION_TIME = 43_200_000; // 12 hours
 }
